784347f5da5468852a288b606665c5160c745849
Ivelin Shenkov <ishenkito@gmail.com>
2024-09-18 21:57:02 +0300

    Create protected route

---
+++ server/middleware/protectRoute.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+const protectRoute = async (req, res, next) => {
+
+    try {
+
+        const token = req.cookies.jwt;
+
+        if (!token) {
+            return res.status(401).json({ error: "Unauthorized - No token" });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decoded) {
+            return res.status(401).json({ error: "Unauthorized - Token is not valid" });
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return res.status(401).json({ error: "User not found" });
+        }
+
+        req.user = user;
+
+        // Call next to proceed to the actual route handler
+        next();
+
+    } catch (error) {
+
+        console.log("Error in protectRoute middleware", error.message);
+        res.status(500).json({ error: "Internal server error" });
+
+    }
+}
+
+export default protectRoute;
