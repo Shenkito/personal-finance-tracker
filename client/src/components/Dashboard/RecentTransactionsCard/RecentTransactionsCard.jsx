ec6e6af852d465f47e330a243dd03c2a4a9ede1e
Ivelin Shenkov <ishenkito@gmail.com>
2024-11-05 20:16:34 +0200

    Check if there are transactions , if there are no transactions display "No transactions found"

--- client/src/components/Dashboard/RecentTransactionsCard/RecentTransactionsCard.jsx
+++ client/src/components/Dashboard/RecentTransactionsCard/RecentTransactionsCard.jsx
@@ -3,14 +3,19 @@ const RecentTransactionsCard = ({ transactions }) => {
         <div className="bg-white p-4 rounded-lg shadow-lg flex flex-col justify-between h-[200px]">
             <h2 className="text-lg font-semibold text-gray-700 mb-4">Recent Transactions</h2>
             <div className="flex-1 overflow-auto">
-                <ul className="space-y-2">
-                    {transactions.map((transaction, index) => (
-                        <li key={index} className="text-sm text-gray-600">
-                            <p className="font-semibold">{transaction.category}</p>
-                            {transaction.description} - ${transaction.amount.toFixed(2)}
-                        </li>
-                    ))}
-                </ul>
+                {transactions.length > 0 ? (
+
+                    <ul className="space-y-2">
+                        {transactions.map((transaction, index) => (
+                            <li key={index} className="text-sm text-gray-600">
+                                <p className="font-semibold">{transaction.category}</p>
+                                {transaction.description} - ${transaction.amount.toFixed(2)}
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p className="text-sm text-gray-500">No transactions found</p>
+                )}
             </div>
         </div>
     );
