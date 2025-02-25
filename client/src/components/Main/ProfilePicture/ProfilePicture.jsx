efb5c269fe195023a10b948b8c3e14728ccb2d7a
Ivelin Shenkov <ishenkito@gmail.com>
2025-02-25 21:33:16 +0200

    Update ProfilePicture.jsx

--- client/src/components/Main/ProfilePicture/ProfilePicture.jsx
+++ client/src/components/Main/ProfilePicture/ProfilePicture.jsx
@@ -4,7 +4,7 @@ const ProfilePicture = ({ className }) => {
     const { authUser } = useAuthContext();
 
     const profilePicUrl = authUser?.profilePicture
-        ? `http://localhost:5000/uploads/${authUser.profilePicture}`
+        ? `https://your-app.onrender.com/uploads/${authUser.profilePicture}`
         : "https://i.pinimg.com/736x/c0/74/9b/c0749b7cc401421662ae901ec8f9f660.jpg";
 
     return (
