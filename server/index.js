d5d2eb31960473208bd9c2a18cd0d81d0f259b25
Ivelin Shenkov <ishenkito@gmail.com>
2025-03-05 21:51:28 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ dotenv.config();
 const app = express();
 
 // const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 // ensureUploadsFolder()
 
