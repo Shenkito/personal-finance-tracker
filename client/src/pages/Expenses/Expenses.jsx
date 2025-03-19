23a93eb01073e9a8e7890577cb619abe673c14ab
Ivelin Shenkov <ishenkito@gmail.com>
2025-03-19 22:10:07 +0200

    Update Expenses.jsx

--- client/src/pages/Expenses/Expenses.jsx
+++ client/src/pages/Expenses/Expenses.jsx
@@ -1,8 +1,9 @@
 const Expenses = () => {
     return (
-        <div className="">
-            <h1 className="text-3xl font-bold">Expenses</h1>
-            {/* Add more expense-specific content here */}
+        <div className="p-4 sm:p-6 lg:p-8">
+            {/* Expenses Header */}
+            <h1 className="text-3xl font-bold mb-6 text-gray-800">Expenses</h1>
+            <p>Under development</p>
         </div>
     );
 };
